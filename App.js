9740e20b0a9f98078db1658770eaa2e4b77fe9f6
Fandi Agus Riyanto <fandiagusriyanto@Fandis-MacBook-Pro.local>
2022-10-13 14:55:05 +0700

    switch screen :sparkles:

--- App.js
+++ App.js
@@ -1,34 +1,45 @@
-import { LinearGradient } from 'expo-linear-gradient';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, ImageBackground } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import { useState } from 'react'
+import { StyleSheet, ImageBackground } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
 
-export default function App() {
+import StartGameScreen from './screens/StartGameScreen'
+import GameScreen from './screens/GameScreen'
+import { StatusBar } from 'expo-status-bar'
+
+const App = () => {
+  const [userNumber, setUserNumber] = useState()
+
+  const pickedNumberHandler = (pickedNumber) => setUserNumber(pickedNumber)
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
+
+  if (userNumber) {
+    screen = <GameScreen />
+  }
+
   return (
     <>
       <StatusBar style='light' />
-      <LinearGradient
-        colors={['#4e0329', '#ddb52f']}
-        style={styles.rootScreen}
-      >
+      <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
         <ImageBackground
           source={require('./assets/images/background.png')}
           resizeMode="cover"
           style={styles.rootScreen}
           imageStyle={styles.backgroundImage}
         >
-          <StartGameScreen />
+          {screen}
         </ImageBackground>
       </LinearGradient>
     </>
-  );
+  )
 }
+export default App
 
 const styles = StyleSheet.create({
   rootScreen: {
-    flex: 1
+    flex: 1,
   },
   backgroundImage: {
-    opacity: 0.15
-  }
-});
+    opacity: 0.15,
+  },
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,85 +1,85 @@
 import { useState } from 'react'
-import { Alert, StyleSheet, TextInput, View } from 'react-native'
+import { TextInput, View, StyleSheet, Alert } from 'react-native'
+
 import PrimaryButton from '../components/PrimaryButton'
 
-const StartGameScreen = () => {
-    const [enteredNumber, setEnteredNumber] = useState('')
+const StartGameScreen = ({onPickNumber}) => {
+  const [enteredNumber, setEnteredNumber] = useState('')
 
-    const numberInputHandler = (enteredText) =>  setEnteredNumber(enteredText)
+  const numberInputHandler = (enteredText) => setEnteredNumber(enteredText)
 
-    const resetInputHandler = () => setEnteredNumber('')
+  const resetInputHandler = () => setEnteredNumber('')
 
-    const confirmInputHandler = () => {
-        const choosenNumber = Number(enteredNumber)
-         if (isNaN(choosenNumber) || choosenNumber <= 0 || choosenNumber > 99) {
-            Alert.alert(
-                'Invalid Number',
-                'Number should to be a number between 1 and 99',
-                [{
-                    text: 'Okay',
-                    style: 'destructive',
-                    onPress: resetInputHandler
-                }]
-            )
-            return
-         }
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredNumber)
 
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid number!',
+        'Number has to be a number between 1 and 99.',
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+      )
+      return
     }
 
-    return (
-        <View style={styles.inputContainer}>
-            <TextInput
-                style={styles.numberInput}
-                maxLength={2}
-                keyboardType="number-pad"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={numberInputHandler}
-                value={enteredNumber}
-            />
-            <View style={styles.buttonsContainer}>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
-                </View>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
-                </View>
-            </View>
+    onPickNumber(chosenNumber)
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
-    )
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  )
 }
+
 export default StartGameScreen
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 100,
-        marginHorizontal: 24,
-        padding: 16,
-        backgroundColor: '#3b021f',
-        borderRadius: 8,
-        elevation: 4,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.25,
-      },
-      numberInput: {
-        height: 50,
-        width: 50,
-        fontSize: 32,
-        borderBottomColor: '#ddb52f',
-        borderBottomWidth: 2,
-        color: '#ddb52f',
-        marginVertical: 8,
-        fontWeight: 'bold',
-        textAlign: 'center',
-      },
-      buttonsContainer: {
-        flexDirection: 'row',
-      },
-      buttonContainer: {
-        flex: 1
-      }
+  inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: '#3b021f',
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
+  },
 })
