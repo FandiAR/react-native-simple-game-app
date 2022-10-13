365e0182dc4370e5c1b490d391dcb15e29314846
Fandi Agus Riyanto <fandiagusriyanto@Fandis-MacBook-Pro.local>
2022-10-13 15:30:24 +0700

    respecting device screen restrictions with the SafeAreaView :sparkles:

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,23 @@
-const GameScreen = () => {
+import { StyleSheet, Text, View } from 'react-native'
 
+const GameScreen = () => {
+    return (
+        <View style={styles.screen}>
+            <Text>Opponent's Guess</Text>
+            {/* GUESS */}
+            <View>
+                <Text>Higher or lower?</Text>
+                {/* + - */}
+            </View>
+            {/* <View>LOG ROUNDS</View> */}
+        </View>
+    )
 }
 export default GameScreen
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 24
+    }
+})
