75327aff192a59182350a134ca45661c30dfc48f
Fandi Agus Riyanto <fandiagusriyanto@Fandis-MacBook-Pro.local>
2022-10-20 14:43:30 +0700

    creating using displaying random number :sparkles:

--- components/ui/PrimaryButton.js
+++ components/ui/PrimaryButton.js
@@ -1,5 +1,6 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
-import Colors from '../constants/colors'
+import Colors from '../../constants/colors'
+
 
 const PrimaryButton = ({ children, onPress }) => (
     <View style={styles.buttonOuterContainer}>
